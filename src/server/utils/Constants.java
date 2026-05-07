6f93bdd316af3fef6745e0463cdc5eee678686e5
yanavlitv <yana.v.litv@gmail.com>
2026-05-07 17:55:44 +0300

    Add TIMEOUT_MS constant to Constants class

--- src/server/utils/Constants.java
+++ src/server/utils/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final int PORT = 2424;
     public static final int BUFFER_SIZE = 8192;
     public static final String DEFAULT_FILE_NAME = "./data/pickme.json";
+    public static final int TIMEOUT_MS = 30000; 
 }
